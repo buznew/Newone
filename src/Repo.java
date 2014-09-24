0ecf3d99b7964fc7b3fe0d4625b8c6febf12eb7b
Rajvaavel <rajkumar@vaavel.com>
2014-09-24 16:58:55 +0530

    commit

---
+++ src/Repo.java
@@ -0,0 +1,12 @@
+
+public class Repo {
+	
+	public static void main(String args[])
+	{
+		System.out.println("hai madam");
+		System.out.println("hai sundari and anitha");
+		System.out.println("hai sundari");
+		System.out.println("hai raj nd suresh");
+	}
+
+}
